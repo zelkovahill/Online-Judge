94b5cbbc8f06f6ad0f741e24c8c5d7beb39c8052
Chang WooMin <changwoomin426@gmail.com>
2024-06-26 08:15:30 +0900

    [goormlevel] Title: Byte, Time: 0.00 ms, Memory: 3.33 MB, Difficulty: 1 -BaekjoonHub

---
+++ goormlevel/242354/1. Byte/Byte.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	
+	string s;
+	cin>>s;
+	int sLength=s.size();
+	
+	cout<<sLength;
+}
