7bbd897cdf5cbd47c4d267ecb84f86f056419504
Chang WooMin <changwoomin426@gmail.com>
2024-09-03 21:25:29 +0900

    [Bronze V] Title: SUAPC 2024 Winter, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32154. SUAPC 2024 Winter/SUAPC 2024 Winter.cc
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+  int N;
+
+  cin >> N;
+
+  switch (N)
+  {
+  case 1:
+    cout << "11\n";
+    cout << "A B C D E F G H J L M";
+    break;
+  case 2:
+    cout << "9\n";
+    cout << "A C E F G H I L M";
+    break;
+  case 3:
+    cout << "9\n";
+    cout << "A C E F G H I L M";
+    break;
+  case 4:
+    cout << "9\n";
+    cout << "A B C E F G H L M";
+    break;
+  case 5:
+    cout << "8\n";
+    cout << "A C E F G H L M";
+    break;
+  case 6:
+    cout << "8\n";
+    cout << "A C E F G H L M";
+    break;
+  case 7:
+    cout << "8\n";
+    cout << "A C E F G H L M";
+    break;
+  case 8:
+    cout << "8\n";
+    cout << "A C E F G H L M";
+    break;
+  case 9:
+    cout << "8\n";
+    cout << "A C E F G H L M";
+    break;
+  case 10:
+    cout << "8\n";
+    cout << "A B C F G H L M";
+    break;
+  }
+}
