8de12135e764ab422c27619bf72190c785db01e1
Chang WooMin <changwoomin426@gmail.com>
2024-09-15 20:46:59 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+// 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요
+char *solution(const char *phone_number)
+{
+  int len = strlen(phone_number);
+
+  char *answer = (char *)malloc((len + 1) * sizeof(char));
+
+  for (int i = 0; i < len; i++)
+  {
+    answer[i] = i < len - 4 ? '*' : phone_number[i];
+  }
+
+  answer[len] = '\0';
+  
+  return answer;
+}
