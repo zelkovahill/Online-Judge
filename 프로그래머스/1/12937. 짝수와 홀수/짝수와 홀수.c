4bb46391d0c21f5f55bf202af701e4d79e801be4
Chang WooMin <changwoomin426@gmail.com>
2024-09-15 20:48:42 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.01 ms, Memory: 4.53 MB -BaekjoonHub

--- 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.c
+++ 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.c
@@ -6,7 +6,7 @@ char* solution(int num) {
     // 리턴할 값은 메모리를 동적 할당해주세요
     // 알파벳 문자 하나당 1바이트
     // "Even" 4바이트 기준
-    char* answer = (char*)malloc(4*sizeof(char));
+    char* answer = (char*)malloc(num*sizeof(char));
     
     answer = num % 2 == 0 ? "Even" : "Odd";
     
