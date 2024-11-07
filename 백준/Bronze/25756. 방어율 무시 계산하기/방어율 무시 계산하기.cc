e95c21f3817874cef2e14c03ee90632850531aca
Chang WooMin <changwoomin426@gmail.com>
2024-11-07 23:26:02 +0900

    [Bronze III] Title: 방어율 무시 계산하기, Time: 24 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/25756. 방어율 무시 계산하기/방어율 무시 계산하기.cc
+++ 백준/Bronze/25756. 방어율 무시 계산하기/방어율 무시 계산하기.cc
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <iomanip>
 #define FAST                          \
     ios_base::sync_with_stdio(false); \
     cin.tie(NULL);                    \
@@ -22,9 +21,7 @@ int main()
 
         ERA = 1 - (1 - ERA) * (1 - a / 100.0);
 
-        cout.setf(ios::fixed);
-        cout << setprecision(6);
-        cout << fixed << setprecision(6) << ERA * 100 << "\n";
+        printf("%.6f\n", ERA * 100);
     }
     return 0;
 }
